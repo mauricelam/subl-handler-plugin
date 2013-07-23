76e97ffa35c8d0a736ad911784157c14800be8ea
Yuk Yin Lam <yukyin@nealmac-t430.thefacebook.com>
2013-07-23 00:13:31 -0700

    Moved subl path to plugin settings

--- subl_handler.py
+++ subl_handler.py
@@ -7,14 +7,23 @@ class RegisterSublHandlerCommand(sublime_plugin.ApplicationCommand):
 
     def __init__(self):
         sublime_plugin.ApplicationCommand.__init__(self)
+        self.settings = sublime.load_settings('SublHandler.sublime-settings')
+        self.settings.add_on_change('subl', self.on_settings_change)
+
+    def on_settings_change(self):
+        path = self.settings.get('path', '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl')
+        appPath = os.path.join(sublime.packages_path(), 'SublHandler/SublHandler.app')
+        os.system('open "' + appPath + '" --args -setPath "' + path + '"')
 
     def run(self):
-        os.system('open SublHandler.app')
+        appPath = os.path.join(sublime.packages_path(), 'SublHandler/SublHandler.app')
+        os.system('open "' + appPath + '"')
 
 try:
     with open('urlregister'):
         pass
 except IOError:
+    # On first run
     RegisterSublHandlerCommand().run()
     with open('urlregister', 'w') as f:
         f.write('yes')
